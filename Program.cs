5992baa1ef8bf8c30d731fa608aeda51446ccc4e
Yogabharathi3 <yogabharathi76@gmail.com>
2024-04-01 16:19:11 +0530

    update

--- Program.cs
+++ Program.cs
@@ -3,21 +3,22 @@ public class Pattern
 {
     public static void Main()
     {
-      int usage;
-      int [][] jaggedArray = new int[4][]
-      
-        jaggedArray[0]=new int[4];
-        jaggedArray[1]=new int[5];
-        jaggedArray[2]=new int[6];
-        jaggedArray[3]=new int[7];
-      for(int i=0;i<jaggedArray.Length;i++)
-      {
-        for(int j=0;jaggedArray[i].Length;j++)
-          {
-            usage=i*j+60;
-            
-          }
-      }
-        Console.WriteLine("CPU usage of {0} node is {1} %", i + 1,usage);
-   }
+        int usage;
+        int[][] jaggedArray = new int[4][];
+
+
+        jaggedArray[0] = new int[4];
+        jaggedArray[1] = new int[5];
+        jaggedArray[2] = new int[6];
+        jaggedArray[3] = new int[7];
+        for (int i = 0; i < jaggedArray.Length; i++)
+        {
+            for (int j = 0;j< jaggedArray[i].Length; j++)
+            {
+                usage = i * j + 60;
+                Console.WriteLine("CPU usage of {0} node is {1} %", i + 1, usage);
+            }
+        }
+        
+    }
 }
